68f82c2e8adcb56fd7bc0481f5032c1c182c10f4
FacingGit (FacingNoOneEXE) <72046783+FacingGit@users.noreply.github.com>
2022-06-13 22:31:22 -0400

    Create Spy x Family.lua

---
+++ Spy x Family.lua
@@ -0,0 +1,9 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 9*60 + 25 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- tempo overides the delay
+
+shared.scr = [[
+[WT60u] [8e%QI][7eyp] [3qryO] I O I[6O] 0 u [6*W] O I O [8%QI]O % [?u] [8QT] Y[*Wru] [*Wr] u I [*Wr] O [e(T]p % [ta] [Q8r] [YO] % ?[*W6u] S [Q70a] O[*Wru] D [$^0f] I o I[W60u] 0 [Wr] T w W Qa 7 [rQS] 7 o O I[6*WS] 6 [wa] W Q [6*W]O % ? [Q8ru] I O[*Wru] T * [wo] [WO] [ra] O [0ryQI]a 0 [3S] [Q7ef] [^WD][60W] [60W] S 6 [W0a] 6 S a 6@ [wEI] p ( [@O]$ * [0e] I $ [*a] [79w]9 [3I] [7w] u [7WI] O I[6WTO] u [OSL] I [Oak] I[t%QO] u w W Q [0T] [*Y]u [*Wr] u * [%I] ! O [@*Ip]@ a [%Q] I o [%I] [?u][*W6u] S [Q70a] O[*Wru] D [$^0f] I o I[W60u] [0I] Qa 7 [rQS] 7 o O I[6*WS] 6 [wa] W Q [6*W]O % ? [Q8ru] I O[*Wru] T * w W r [Q0ry]a 0 [3S] [Q7ef] [^WD][60W] [60W] S 6 [W0a] 6 S a 6@ [wEI] p ( [@O]$ * [0e] S $ [*a] [79w]9 [3a] [7w] i I [7W] u[WT60u] [ey%QI][7eyQO] S a [9y^WO] I u[6*WO] 6 w W Q [6*W]O % a ? S [Q8rf] D a[r*WS] * w W r [fx] [Q0ryfx][fx] 0 [3GC] [Q7ev] [HV] [^Wfx][0p$S] [%0ua][6WS] D f G [7eQS] D a [W70f]3 ? [?^Oif] f f [IG][?6] [Of] S [WO0f] f f 6[pIS] 6 ? [8QD] [uf] [29uf] [aIG][!*] [uf] [TS] [O0ruf] [Ouf] 0[yOS] 0 3 [9dWOH] [?^OH] [ak][?6] [pj] [OH] [0WG] [uf] 6[sIG] 6 ? [Q8sf] [DG] W H[(TSej] [sWIH][Q7edj] k [dh] ^ H [9W] [^G] [6?G][O6f] [6S] [WO0f] [6f] f 6[pIS] 6 ? [8QD] [uf] [29uf] [aIG][!*] [uf] [TS] [O0ruf] [Ouf] [Q9e][ypS] 7 ? [9dWOH] [?^OH] [ak][?6] [pj] [OH] [0WG] [uf] 6[sIG] 6 ? [Q8sf] [DG] W H[(TSej] [sWIH][7eQH] [pj] [ak] [dOH] [9^W] [SL] [ak][60W] 6 [WO0a] [6pS] [9raD]f 9 [2uO] [W0r] S %[$T0a] e [ruS] w W [0OH] [*uf][Q7es] a [(Teu][0rYO]V b V C x Z k G Hf D a S O I u Ou T r e W 0 Q 0[3(] 0 ( 0 ( 0 ( 0 ( 0 ( 0 (
+]]
